cfde720f7a2518a35fc98a8e5bd4ef586017efda
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-02-10 20:21:28 +0000

    lint

--- src/frequency/frequency.test.ts
+++ src/frequency/frequency.test.ts
@@ -4,7 +4,6 @@ describe("frequencies", () => {
   describe("single frequency", () => {
     test("a single word sentence returns 100% frequency for that word", () => {
       const sentence = "why?";
-
       const frequency = singleFrequencyCount(sentence, "why");
 
       expect(frequency).toBe(1);
@@ -12,7 +11,6 @@ describe("frequencies", () => {
 
     test("a two word sentence returns 50% frequency for one of the words (the words are different)", () => {
       const sentence = "why not?";
-
       const frequency = singleFrequencyCount(sentence, "not");
 
       expect(frequency).toBe(0.5);
@@ -20,7 +18,6 @@ describe("frequencies", () => {
 
     test("percentages are limited to four decimal places", () => {
       const sentence = "why not me?";
-
       const frequency = singleFrequencyCount(sentence, "why");
 
       expect(frequency).toEqual(0.3333);
