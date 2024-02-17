020b7bcb51e335de7fc7420736ed0fd052165e9b
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-02-17 12:35:04 +0000

    allow sentence components to be protected from the punctuation remover

--- src/utils/sentenceComponents/sentenceComponents.test.ts
+++ src/utils/sentenceComponents/sentenceComponents.test.ts
@@ -15,4 +15,22 @@ describe("sentence components", () => {
     const sentenceComponents = getSentenceComponents("the big brown fat cat");
     expect(sentenceComponents).toEqual(["the", "big", "brown", "fat", "cat"]);
   });
+
+  describe("word preservation", () => {
+    test("a phrase can be protected from the sentence splitter", () => {
+      const fullSentence = `Lock & Co`;
+      const components = getSentenceComponents(fullSentence, ["Lock & Co"]);
+
+      expect(components).toEqual(["Lock & Co"]);
+    });
+    test("Multiple phrases can be protected from the sentence splitter", () => {
+      const fullSentence = `Lock & Co, abc:xyz`;
+      const components = getSentenceComponents(fullSentence, [
+        "Lock & Co",
+        "abc:xyz",
+      ]);
+
+      expect(components).toEqual(["Lock & Co", "abc:xyz"]);
+    });
+  });
 });
--- src/utils/sentenceComponents/sentenceComponents.ts
+++ src/utils/sentenceComponents/sentenceComponents.ts
@@ -1,4 +1,29 @@
+import {
+  ProtectionMap,
+  addWordProtection,
+  buildProtectionMap,
+  removeWordProtection,
+} from "../protection/protection";
 import { punctuationRemover } from "../punctuation/punctuation";
 
-export const getSentenceComponents = (text: string): string[] =>
-  punctuationRemover(text).split(" ");
+export const getSentenceComponents = (
+  text: string,
+  protectionList?: string[]
+): string[] => {
+  const protectionMap: ProtectionMap = new Map();
+  let content = text
+  if (protectionList) {
+    buildProtectionMap(protectionList, protectionMap);
+    content=addWordProtection(protectionMap, text);
+  }
+  const sentenceComponents = punctuationRemover(content, protectionList).split(
+    " "
+  );
+  const unprotectedSentenceComponents = [];
+  for (const sentenceComponent of sentenceComponents) {
+    unprotectedSentenceComponents.push(
+      removeWordProtection(protectionMap, sentenceComponent)
+    );
+  }
+  return unprotectedSentenceComponents;
+};
