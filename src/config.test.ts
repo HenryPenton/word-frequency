c476e72cba28eadf5051a5cda6fc3a73fb53bec3
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-03-09 11:54:39 +0000

    add config test

---
+++ src/config.test.ts
@@ -0,0 +1,6 @@
+import { defaultConfig } from './config';
+describe('default config', () => {
+  test('default config is empty', () => {
+    expect(defaultConfig).toStrictEqual({ protectionList: [] });
+  });
+});
